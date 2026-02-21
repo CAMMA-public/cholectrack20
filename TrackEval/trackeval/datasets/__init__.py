295d42f660c73c1055eb00f191554bbc12f6c165
Nwoye Chinedu <nwoye.chinedu@gmail.com>
2026-02-21 13:57:23 -0800

    Add CholecTrack2DBox to dataset imports

--- TrackEval/trackeval/datasets/__init__.py
+++ TrackEval/trackeval/datasets/__init__.py
@@ -15,3 +15,4 @@ from .youtube_vis import YouTubeVIS
 from .head_tracking_challenge import HeadTrackingChallenge
 from .rob_mots import RobMOTS
 from .person_path_22 import PersonPath22
+from .cholectrack import CholecTrack2DBox
